7175aa8b6659a8c3d1dc5878931dfcae4b2a1165
Ucheawaji Anne Dienye <dienyeucheawaji93@gmail.com>
2025-04-28 12:35:11 +0100

    operating system

---
+++ operating system.py
@@ -0,0 +1,9 @@
+#coding
+import sys
+
+# Program to report the operating system platform
+def main():
+    print(f"Operating System Platform: {sys.platform}")
+
+if __name__ == "__main__":
+    main()
